bfe74e89771a0ae4c032077f3f93e578731cf2c0
zorbaxx <101431748+zorbaxx@users.noreply.github.com>
2022-12-07 20:03:41 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,5 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+RegisterCommand('pruebasyoutube', function()
+    ESX.ShowNotification('Notificacion de prueba')
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zorbax'
+description 'Script basico para crear comandos'
+version '1.0'
+
+client_script 'client.lua'
